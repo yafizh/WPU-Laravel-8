5d3223401076180b38b23dfd7f5e543f6972f7ba
yafizh <a.chairil.yafizh@gmail.com>
2022-03-01 21:26:23 +0800

    struktur folder, routes & view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Sandhika Galih",
+        "email" => "sandhikagalih@unpas.ac.id",
+        "image" => "sandhika.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
