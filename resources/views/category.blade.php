2230ed72ae3823061dcdef52039130ceace1a151
yafizh <a.chairil.yafizh@gmail.com>
2022-03-03 18:50:19 +0800

    post category * eloquent relationship

---
+++ resources/views/category.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/main')
+
+@section('container')
+    <h1>Post Category: {{ $category }}</h1>
+    @foreach ($posts as $post)
+        <article>
+            <h2>
+                <a href="/post/{{ $post->slug }}">{{ $post->title }}</a>
+            </h2>
+            <p>{{ $post->excerpt }}</p>
+        </article>
+    @endforeach
+@endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,10 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-       {!! $post->body !!}
+
+        <p>By. Sandhika Galih in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
+        {!! $post->body !!}
     </article>
 
     <a href="/posts">Back to Posts</a>
