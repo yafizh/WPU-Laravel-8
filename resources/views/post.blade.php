a0699113930031bab403124cbffd57f09f48753a
yafizh <a.chairil.yafizh@gmail.com>
2022-03-06 03:17:11 +0800

    redesign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts/main')
 
 @section('container')
-    <h2>{{ $post->title }}</h2>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h1 class="mb-3">{{ $post->title }}</h1>
 
-    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a
-            href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
-    </p>
+                <p>By. <a href="/authors/{{ $post->author->username }}"
+                        class="text-decoration-none">{{ $post->author->name }}</a> in <a
+                        href="/categories/{{ $post->category->slug }}"
+                        class="text-decoration-none">{{ $post->category->name }}</a>
+                </p>
 
-    {!! $post->body !!}
+                <img src="https://random.imagecdn.app/1200/400" alt="{{ $post->category->name }}" class="img-fluid">
 
-    <a href="/posts" class="d-block mt-3">Back to Posts</a>
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+
+                <a href="/posts" class="d-block mt-3">Back to Posts</a>
+            </div>
+        </div>
+    </div>
 @endsection
