ec5504dd3ab6d687c865d3eb1fa939c0a7769506
yafizh <a.chairil.yafizh@gmail.com>
2022-03-08 02:15:41 +0800

    searching & pagination

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Sandhika Galih",
         "email" => "sandhikagalih@unpas.ac.id",
         "image" => "sandhika.jpg"
@@ -41,20 +43,5 @@ Route::get('/categories', function () {
         'title' => 'Post Categories',
         "active" => "categories",
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post by Category: " . $category->name,
-        "active" => "categories",
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post by Author: " . $author->name,
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
