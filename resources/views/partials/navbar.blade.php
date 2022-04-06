4fc6dd122b6a850053c360e5819070bd0895f0cc
yafizh <a.chairil.yafizh@gmail.com>
2022-04-06 11:26:06 +0800

    user login & middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,10 +21,33 @@
                         href="/categories">Categories</a>
                 </li>
             </ul>
+
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a href="/login" class="nav-link {{ $active == 'login' ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-                </li>
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/dashboard"><i
+                                        class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form action="/logout" method="post">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-right"></i> Logout</a></button>
+                                </form>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link {{ $active == 'login' ? 'active' : '' }}"><i
+                                class="bi bi-box-arrow-in-right"></i> Login</a>
+                    </li>
+                @endauth
             </ul>
         </div>
     </div>
