d433568895e28323ad7beee528a2e55cfbd188e0
yafizh <a.chairil.yafizh@gmail.com>
2022-04-14 02:07:18 +0800

    validation & insert post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,8 @@ use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
+
 
 class DashboardPostController extends Controller
 {
@@ -41,7 +43,19 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request);
+        $validatedData = $request->validate([
+            'title' => ['required', 'max:255'],
+            'slug' => ['required', 'unique:posts'],
+            'category_id' => ['required'],
+            'body' => ['required'],
+        ]);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200, '...');
+
+        Post::create($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -7,6 +7,13 @@
 
     <div class="table-responsive col-lg-8">
         <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
+
+        @if (session()->has('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
+
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
@@ -23,7 +30,8 @@
                         <td>{{ $post->title }}</td>
                         <td>{{ $post->category->name }}</td>
                         <td>
-                            <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
+                            <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span
+                                    data-feather="eye"></span></a>
                             <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
                             <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
                         </td>
